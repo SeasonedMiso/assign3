de24ebe9ee4bdd1fb25b324c33e317697d897c76
Miso <zakwrn@gmail.com>
2022-09-08 17:06:02 +0200

    Removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -129,20 +129,5 @@ int main(int argc, char *argv[])
     cout << "Components within filter range: " << a->filterComponentsBySize(3, 200) << endl;
     string writeSuccess = (a->writeComponents(args.writeArg)) ? "Write success" : "Write failed";
     cout << writeSuccess;
-    // cout << "Write succesful?: " << a->writeComponents(args.writeArg);
     delete (a);
 }
-
-// findcomp[options]<inputPGMfile>
-// where<inputPGMfile> is a valid PGM image and valid options
-
-// - s<int><int>(-s min max); set the minimum and maximum valid components size
-// [min default = 3, max = image width*img height i.e. unlimited]
-
-// -t <int> set the threshold for component detection (default=128, limit to [0. . . 255])
-
-// -p print out all the component data (entries do not have to be sorted) as well as the total
-// component number and the sizes of the smallest and largest components.
-
-// -w <outPGMfile> write out all retained components (as foreground/background pixels)
-// to a new PGM file, <outPGMfile>
